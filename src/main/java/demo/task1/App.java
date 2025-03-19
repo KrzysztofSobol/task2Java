3021aed8c2fe7fd307c7c655ec661642423672d7
Kex <krzysieksobol9@gmail.com>
2025-03-19 16:13:59 +0100

    Logging done

--- src/main/java/demo/task1/App.java
+++ src/main/java/demo/task1/App.java
@@ -1,9 +1,30 @@
 package demo.task1;
 
-/**
- *
- */
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
 public class App {
+    private static final Logger logger = Logger.getLogger(App.class.getName());
+
+    static {
+        try {
+            InputStream configFile = App.class.getClassLoader().getResourceAsStream("logging.properties");
+
+            if (configFile == null) {
+                throw new IOException("Could not find logging.properties in resources");
+            }
+
+            LogManager.getLogManager().readConfiguration(configFile);
+            configFile.close();
+
+            logger.info("Logging configuration loaded successfully");
+        } catch (IOException e) {
+            logger.severe("Could not load logging configuration:");
+            throw new RuntimeException(e);
+        }
+    }
 
     public static void main(String[] args) {
         System.out.println("start");
